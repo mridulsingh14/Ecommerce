914d0803cc8b618f8ee30714f77c31b0532b1840
Mridul Singh <138520000+mridulsingh14@users.noreply.github.com>
2024-08-01 21:00:15 +0530

    update

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='discriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+            Welcome to MSKART, your ultimate online fashion destination. 
+            Explore a curated collection of trendy apparel for men, women, and kids, 
+            featuring the latest styles and timeless classics.
+            Enjoy seamless shopping, exclusive deals, and fast delivery right to your doorstep.
+            Whether you're looking for chic everyday wear or standout pieces for special occasions, MSKART has you covered. 
+            Elevate your wardrobe and express your unique style with MSKART today!
+        </p>
+        <p>
+            Welcome to MSKART, your ultimate online fashion destination. 
+            Explore a curated collection of trendy apparel for men, women, and kids, 
+            featuring the latest styles and timeless classics.
+            Enjoy seamless shopping, exclusive deals, and fast delivery right to your doorstep.
+            Whether you're looking for chic everyday wear or standout pieces for special occasions, MSKART has you covered. 
+            Elevate your wardrobe and express your unique style with MSKART today!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
